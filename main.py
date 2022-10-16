d0cbc0afc88c0bd7bd4ee9d50b9d0d27e16c058c
Hussein Ali <111470519+thisishusseinali@users.noreply.github.com>
2022-10-16 06:56:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
 
-        show_extract_button(path)
-    except:
-        pass  
-uploadbtn = Button(root,text="Upload an image",command=upload,bg="#2f2f77",fg="gray",height=2,width=20,font=('Times',15,'bold')).pack()
-newline.configure(text='\n')
-newline.pack()
-uploaded_img.pack()
-
-root.mainloop()

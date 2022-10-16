3231377cd6af5af9021bdc5218d9f29a04bddaab
Hussein Ali <111470519+thisishusseinali@users.noreply.github.com>
2022-10-16 06:54:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-    Sample_img = cv2.resize(Actual_image,(400,350))
     Image_ht,Image_wd,Image_thickness = Sample_img.shape
     Sample_img = cv2.cvtColor(Sample_img,cv2.COLOR_BGR2RGB)
     texts = pytesseract.image_to_data(Sample_img) 
